63414feeb7d1272113cacdfc143557e7111489af
tzipi-mintzer <m0527145332@gmail.com>
2023-04-20 23:27:54 +0300

    first commit

---
+++ src/service.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import dotenv from 'dotenv'
+dotenv.config();
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+export default {
+  getTasks: async() => {
+    console.log("get", process.env.REACT_APP_API_URL)
+    const result = await axios.get(`/items`)
+    return result.data;
+  },
+  addTask: async (name) => {
+    console.log('addTask')
+    const result = await axios.post(`/items`, { name, IsComplete: false })
+  },
+  setCompleted: async (id, isComplete) => {
+    console.log('setCompleted', { id, isComplete });
+    const result = await axios.put(`/items`, { id, isComplete });
+  },
+  deleteTask: async (id) => {
+    console.log('deleteTask')
+    await axios.delete(`/items/${id}`);
+  }
+};
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    console.log("mistake", error);
+    return Promise.reject(error);
+  }
+);
